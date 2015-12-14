13758761b83c6289f3ac6ba3fee8e92d6d1fdaa6
repetti <repetti@users.noreply.github.com>
2015-12-14 16:02:49 +0100

    compareJson

--- main/src/main/java/org/repetti/utils/JsonHelper.java
+++ main/src/main/java/org/repetti/utils/JsonHelper.java
@@ -392,4 +392,28 @@ public class JsonHelper {
         throw new UtilsException(UtilsException.Type.PARAMETERS, field + " not found", null);
     }
 
+    public static String compareJson(JsonNode from, JsonNode to) {
+        StringBuilder sb = new StringBuilder();
+        compareJson(sb, "", from, to);
+        return sb.toString();
+    }
+
+    public static void compareJson(StringBuilder sb, String prefix, JsonNode from, JsonNode to) {
+        if (from == to) {
+            sb.append(prefix).append("are equal");
+            return;
+        }
+        if (from == null) {
+            sb.append(prefix).append("added");
+            return;
+        }
+        if (to == null) {
+            sb.append(prefix).append("removed");
+            return;
+        }
+        if (from.isObject() && to.isObject()) {
+            sb.append(prefix).append("removed");
+            return;
+        }
+    }
 }
